a15e49ed8192567cad1160b09f2df8aa1001b184
Abdul Majid <helloabdulmajid@gmail.com>
2022-04-08 11:35:52 +0530

    First Commit

---
+++ am.js
@@ -0,0 +1,19 @@
+//Get the button
+var mybutton = document.getElementById("amTopBtn");
+  
+ 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
